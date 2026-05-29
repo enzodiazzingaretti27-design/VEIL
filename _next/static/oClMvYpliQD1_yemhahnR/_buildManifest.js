2b153efa9cc90fa561be6b5ea3a8a6c7ee1ab9fb
enzodiazzingaretti27-design <enzodiazzingaretti27-design@users.noreply.github.com>
2026-05-29 04:47:07 -0300

    Updates

---
+++ _next/static/oClMvYpliQD1_yemhahnR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/VEIL//_next/:path+",
+        "destination": "/VEIL/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
